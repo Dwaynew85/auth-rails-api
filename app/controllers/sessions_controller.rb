09c0f1bc547c2cbbe480f58e1ba5dd24d5b3736e
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-22 00:10:00 -0500

    updated create, logged_in, and destory actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,46 @@
 class SessionsController < ApplicationController
     def create
-        @user = User.find_by(email: params[:user][:email])
+        @user = User.find_by(username: session_params[:username])
 
         if @user && @user.authenticate(params[:user][:password])
-            render json: @user
-        else
-            resp = {
-                error: 'Invalid credentials',
-                details: @user.errors.full_messages
+            session[:user_id] = @user.id
+            render json: {
+                user: UserSerializer.new(@user)
             }
-            render json: resp, status: :unathorized
+        else
+            render json: {
+                status: 401,
+                error: "Could not authenticate your account"
+            }
         end
     end
 
-    def delete 
+    def is_logged_in?
+        @current_user = User.find(session[:user_id]) if session[:user_id]
 
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: UserSerializer.new(@current_user)
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
+
+    def destroy 
+        session.delete(:user_id) # try session..delete :user_id  if error is raised
+        render json: {
+            status: 200,
+            logged_out: true
+        }
+    end
+
+    private
+
+    def session_params
+        params.require(:user).permit(:username, :password)
     end
 end
