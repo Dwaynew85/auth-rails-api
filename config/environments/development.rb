3f5282e48350c4375ae30d9986a4a2aac3e9466c
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-21 23:06:17 -0500

    added configuration and middleware

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.session_store :cookie_store, key: '_interslice_session'
+  
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
