994a6fbe8ffa3e4eaac215b3fc9297249597055a
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-22 00:18:01 -0500

    removed secrets resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :secrets
     validates :email, :username, presence: true, uniqueness: true
     # validates :email, :username, uniqueness: true
 end
